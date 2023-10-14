15bfe4555233bb6f208b9b111f4e4cd9a2f0f1d3
baleBox <bale@bk.ru>
2023-10-14 16:47:42 +0300

    First commit

--- src/AcademyStringCalc/Printer.java
+++ src/AcademyStringCalc/Printer.java
@@ -3,7 +3,7 @@ package AcademyStringCalc;
 public class Printer {
     public void printQuotationMarks(String output) {
         if (output.length() > 40) {
-            System.out.println("\"" + output + "\"" + "...");
+            System.out.println("\"" + output + "...\"");
         } else {
             System.out.println("\"" + output + "\"");
         }
