6ef624bb1f1d4f75d2ccebadb7463724c70f2d9e
baleBox <bale@bk.ru>
2023-10-20 23:07:24 +0300

    Second commit

--- src/AcademyStringCalc/StringCalc.java
+++ src/AcademyStringCalc/StringCalc.java
@@ -3,12 +3,13 @@ package AcademyStringCalc;
 
 import java.io.IOException;
 import java.util.Scanner;
+
 import static java.lang.Integer.*;
 
 public class StringCalc {
     static String expression;
     static char operation;
-   // static String[] input;
+    static String[] input;
 
     public static void main(String[] args) throws IOException, NumberFormatException {
         Scanner scanner = new Scanner(System.in);
@@ -22,7 +23,6 @@ public class StringCalc {
         System.out.println("\n");
         System.out.println("Output: ");
 
-        String[] input;
 
         if (expression.contains("+")) {
             input = expression.split("\\+");
@@ -55,7 +55,6 @@ public class StringCalc {
         if ((input[0].length() > 10) || (input[1].length() > 10)) {
             throw new IOException("Строки должны длинной не более 10 символов.");
         }
-
         //сложение
         if (operation == '+') {
             printer.printQuotationMarks(input[0] + input[1]);
@@ -75,13 +74,18 @@ public class StringCalc {
         } else if (operation == '-') {
             int index = input[0].indexOf(input[1]);
             if (index == -1) {
-                printer.printQuotationMarks(input[0]+"-"+input[1]);
+                printer.printQuotationMarks(input[0] + "-" + input[1]);
             } else {
                 String minus = input[0].substring(0, index);
                 minus = minus + input[0].substring(index + input[1].length());
                 printer.printQuotationMarks(minus);
             }
-        } else {                         // деление
+        } else {
+            // деление
+            int num1 = parseInt(input[1]);
+            if (num1 < 1 || num1 > 10) {
+                throw new IOException("Калькулятор принимает на вход числа от 1 до 10 включительно.");
+            }
             int divide = input[0].length() / parseInt(input[1]);
             String minusdivide = input[0].substring(0, divide);
             printer.printQuotationMarks(minusdivide);
